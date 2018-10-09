aff49afc29f6dc9cb8a98db7731488ceead339a4
David Dupplaw <david@dupplaw.me.uk>
2018-10-09 22:17:29 +0100

    feat: DD: extra bits on estimated story component

--- frontend/src/app/estimated-stories/estimated-stories.component.ts
+++ frontend/src/app/estimated-stories/estimated-stories.component.ts
@@ -7,9 +7,10 @@ import {EstimatedStory} from '../../../../shared/domain/EstimatedStory';
   styleUrls: ['./estimated-stories.component.scss']
 })
 export class EstimatedStoriesComponent implements OnInit {
-  @Input() estimatedStories: {[storyId: string]: EstimatedStory};
+  @Input() estimatedStories: { [storyId: string]: EstimatedStory };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
@@ -18,9 +19,13 @@ export class EstimatedStoriesComponent implements OnInit {
     return Object.keys(this.estimatedStories);
   }
 
+  whoPartook(storyId: string) {
+    return Object.keys(this.estimatedStories[storyId].estimates);
+  }
+
   averageSize(storyId: string): number {
     const whoVoted = Object.keys(this.estimatedStories[storyId].estimates);
-    return whoVoted.map(w => parseInt(this.estimatedStories[storyId].estimates[w].estimate, 10) )
-                   .reduce( (a, b) => a + b ) / whoVoted.length;
+    return whoVoted.map(w => parseInt(this.estimatedStories[storyId].estimates[w].estimate, 10))
+      .reduce((a, b) => a + b) / whoVoted.length;
   }
 }
